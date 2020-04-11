3f16a2467860ba1f7a392f324fb8303c3c9a8474
Sorathia <arif_sorathia@intuit.com>
2020-04-10 21:31:33 -0400

    report html zooom implemented

--- src/main/java/com/lyra/reportsdk/service/ReportService.java
+++ src/main/java/com/lyra/reportsdk/service/ReportService.java
@@ -9,7 +9,10 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.HtmlExporter;
 import net.sf.jasperreports.export.Exporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleHtmlExporterConfiguration;
 import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
+import net.sf.jasperreports.export.SimpleHtmlReportConfiguration;
+import net.sf.jasperreports.export.type.HtmlSizeUnitEnum;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -27,18 +30,18 @@ public class ReportService {
     final JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(beanCollection);
 
     final Map<String, Object> parameters = new HashMap<>();
-    parameters.put("COMPANY_NAME", "ARON Inc");
-    parameters.put("COMPANY_ADDRESS", "Mississauga, Ontario");
-    parameters.put("COMPANY_PHONE", "36637778");
-    parameters.put("COMPANY_FAX", "7878733773");
+    parameters.put("COMPANY_NAME", "Syed Brothers Hardware Inc");
+    parameters.put("COMPANY_ADDRESS", "2345 Spectrum Way, Mississauga, Ontario");
+    parameters.put("COMPANY_PHONE", "+1 366 377 7833");
+    parameters.put("COMPANY_FAX", "+1 787 873 3773");
     parameters.put("POWERED_BY", "NA");
     parameters.put("SUPPORT_EMAIL", "support@lyrasdk.com");
     parameters.put("PERIOD", "Jan 1, 2020 - 31, Jan 2020");
-    parameters.put("PRODUCT_NAME", "Lyra SDK");
+    parameters.put("PRODUCT_NAME", "lyra-sdk");
     parameters.put("COPY_RIGHTS", "All rights reserved by LYRA Sdk");
-    parameters.put("SUPPORTED_BY", "LyraSDK");
-    parameters.put("SYSTEM_USER", "Arif");
-    parameters.put("COMPANY_PHONE_FAX_INFO", "987766667");
+    parameters.put("SUPPORTED_BY", "LYRA-SDK");
+    parameters.put("SYSTEM_USER", "Arif Sorathia");
+    parameters.put("COMPANY_PHONE_FAX_INFO", "+1 987 766 6678");
     parameters.put("EMAIL_WEB", "info@abc.com");
 
     final JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
@@ -47,8 +50,12 @@ public class ReportService {
     final ByteArrayOutputStream out = new ByteArrayOutputStream();
     exporter.setExporterOutput(new SimpleHtmlExporterOutput(out));
     exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-    exporter.exportReport();
 
+    SimpleHtmlReportConfiguration htmlReportConfig = new SimpleHtmlReportConfiguration();
+    htmlReportConfig.setZoomRatio(2.0f);
+    exporter.setConfiguration(htmlReportConfig);
+
+    exporter.exportReport();
     return  new String(out.toByteArray());
   }
 }
